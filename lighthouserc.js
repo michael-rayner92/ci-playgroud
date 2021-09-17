f5069b3c323c794ee872ca548a08e1a03ac90428
Michael Rayner <scetils_michael@hotmail.com>
2021-09-17 23:54:47 +1000

    Fixed typos in lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,10 +7,10 @@ module.exports = {
       preset: 'lighthouse:no-pwa',
       assertions: {
         'csp-xss': 'warn',
-        'categories:performace': ['error', { minScore: 0.93 }],
+        'categories:performance': ['error', { minScore: 0.93 }],
         'categories:accessibility': ['error', { minScore: 0.93 }],
-        'categories:best-practice': ['error', { minScore: 0.9 }],
-        'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 1 }],
       },
     },
   },
